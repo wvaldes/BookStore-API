48a98fc7427e145093e0006f4a8ad493b6840f45
Wesley Valdes <wesley.valdes@gmail.com>
2020-05-26 13:37:41 -0700

    Add project files.

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NLog;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+
+        public HomeController(ILoggerService logger)
+            {
+                _logger = logger;
+            }
+
+        /// <summary>
+        /// Gets Values
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/Home
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Accessed Home Controller");
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Get a value
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET api/Home/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            _logger.LogDebug("Got a value");
+            return "value";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogError("Got an error");
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logger.LogWarn("This is a warning");
+        }
+    }
+}
