8b26a18a84421fee48ec3182f55742a830c58952
Wesley Valdes <wesley.valdes@gmail.com>
2020-05-30 06:49:40 -0700

    Added Get API's for Authors and Author ID

---
+++ BookStore-API/Controllers/AuthorsController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BookStore_API.Contracts;
+using BookStore_API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// Endpoint used to interact with the Authors in the book store's database
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IAuthorRepository _authorRepository;
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(IAuthorRepository authorRepository,
+            ILoggerService logger,
+            IMapper mapper)
+        {
+            _authorRepository = authorRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        /// <summary>
+        /// Get All Authors
+        /// </summary>
+        /// <returns>List of Authors</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAuthors()
+        {
+            try
+            {
+                _logger.LogInfo("Attempted Get All Authors");
+                var authors = await _authorRepository.FindAll();
+                var response = _mapper.Map<IList<AuthorDTO>>(authors);
+                _logger.LogInfo("Successfully got all Authors.");
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return InternalError($"{ e.Message} - {e.InnerException }");
+            }
+        }
+        /// <summary>
+        /// Get an Author by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>An Authors record</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public  async   Task<IActionResult> GetAuthor(int id)
+        {
+            try
+            {
+            _logger.LogInfo($"Attempted to get author with id: {id}");
+            var author = await _authorRepository.FindById(id);
+            if(author == null)
+                {
+                    _logger.LogWarn("Author search got a null request for id: {id}");
+                    return NotFound();
+                }
+            var response = _mapper.Map<AuthorDTO>(author);
+            _logger.LogInfo($"Successfully got author with id: {id}");
+            return Ok(response);
+
+            }
+            catch (Exception e)
+            {
+                return InternalError($"{ e.Message} - {e.InnerException }");
+            }
+        }
+
+        private ObjectResult InternalError (string message)
+        {
+            _logger.LogError(message);
+            return StatusCode(500, "Something went wrong ... again.");
+        }
+    }
+}
--- BookStore-API/Startup.cs
+++ BookStore-API/Startup.cs
@@ -31,6 +31,12 @@ namespace BookStore_API
 
         public IConfiguration Configuration { get; }
 
+
+
+
+
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -63,10 +69,22 @@ namespace BookStore_API
             });
 
             services.AddSingleton<ILoggerService, LoggerService>();
+            services.AddScoped<IAuthorRepository, AuthorRepository>();
 
 
             services.AddControllers();
         }
+
+
+
+
+
+
+
+
+
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
